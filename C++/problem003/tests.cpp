3d9e417cd8462a370809c1da1d2ae899fef6e1cb
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2016-01-27 17:03:31 +0000

    tests.cpp: int now long int

--- C++/problem003/tests.cpp
+++ C++/problem003/tests.cpp
@@ -6,16 +6,16 @@
 #include <cmath>
 #include "ep003.h"
 
-bool IsFactor(int target, int factor)
+bool IsFactor(long int target, long int factor)
 {
     return (target % factor == 0) ? true : false;
 }
 
-bool IsPrime (int factor)
+bool IsPrime (long int factor)
 {
     // uses Fermat's Little Theorem
     // cast to int should be safe as 2^int is an int
     double fermat = std::exp2(factor);
 
-    return ((int)fermat % factor == 2) ? true : false;
+    return ((long int)fermat % factor == 2) ? true : false;
 }
