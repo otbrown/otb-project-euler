9b7fca61401486ac803273b6f59324bdea37e694
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2016-01-27 16:25:23 +0000

    ep003.h: initial commit

---
+++ C++/problem003/ep003.h
@@ -0,0 +1,13 @@
+#ifndef _ep003_h
+#define _ep003_h
+
+// function which returns the largest prime factor of a target, x 
+int LargestPrimeFactor(int x);
+
+// test if a number is integer
+bool IsFactor(int target, int factor);
+
+// test if number is prime
+bool IsPrime(int x);
+
+#endif
