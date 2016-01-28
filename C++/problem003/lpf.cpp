83dcfb2fd65d13f2fd1d5ce636367f26245e18d2
Oliver Thomson Brown <otbrown91@gmail.com>
2016-01-28 17:07:33 +0000

    lpf.cpp: function now operates recursively, accepts new lower bound for search on input

--- C++/problem003/lpf.cpp
+++ C++/problem003/lpf.cpp
@@ -3,19 +3,40 @@
 // Oliver Thomson Brown
 // 2016-01-27
 
+#include <cmath>
 #include "ep003.h"
 
-long int LargestPrimeFactor (long int target)
+long int LargestPrimeFactor (long int target, long int lowerBound)
 {
     long int i;
     long int lpf = -1;
+	long int targetRt; 
 
-    for (i = (target / 2); i > 1; i = i - 2) {
-       if (IsFactor(target, i)) {
-           lpf = i;
-           if (IsPrime(lpf)) break;
-       }
-    }
+	if (lowerBound == 2) {
+		if (IsFactor(target, 2)) {
+			target /= 2;
+			lpf = LargestPrimeFactor(target, 2);
+		} else {
+			lpf = LargestPrimeFactor(target, 3);
+		} 
+	}
 
-    return lpf;
+	if (!IsPrime(target)) {	
+		targetRt = (long int) std::sqrt(target); 
+
+		for (i = lowerBound; i < targetRt; i += 2) {
+			if (IsFactor(target, i)) {
+				if (IsPrime(i)) {
+					target /= i;
+					lpf = LargestPrimeFactor(target, i);
+					return lpf;
+				}
+			}
+		}
+	} else {
+		lpf = target;
+		return lpf;
+	}
+
+	return lpf;
 }
