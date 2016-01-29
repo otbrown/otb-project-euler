375f00c1c36c8744c14021462e32abe1091b72e8
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2016-01-29 14:14:49 +0000

    ep004.h: initial commit, contains IsPali, IntLen, IntSplit

---
+++ C++/problem004/ep004.h
@@ -0,0 +1,13 @@
+#ifndef _ep004_h_
+#define _ep004_h_
+
+// test if integer is palindromic
+bool IsPali(int number);
+
+// find number of digits in an integer
+int IntLen(int number);
+
+// separate digits in an integer
+int* IntSplit(int number, int length);
+
+#endif
